39968792eff9dd1d136e1ed0b07993c41e490240
Scott <scottdomes@gmail.com>
2021-03-16 21:12:46 +0000

    Done hash table

---
+++ chapter-1/balanced_binary_tree.py
@@ -0,0 +1,6 @@
+# Run this file by copying 'python chapter-1/hash_table.py' into the Console box in the bottom righthand corner and hitting 'Enter'.
+
+# Goal: Make a custom hash table implementation in python, using a balanced binary tree
+# See hash_table.py for a simpler version
+
+
--- chapter-1/hash_table.py
+++ chapter-1/hash_table.py
@@ -36,3 +36,44 @@ insert2(hash_table2, 25, 'Twenty-five')
 print(hash_table2)
 # -> [['Ten', 'Twenty'], [], [], [], [], ['Twenty-five'], [], [], [], []]
 
+# Implementation 3: Chaining with search
+
+hash_table3 = [[] for _ in range(10)]
+
+def find_bucket(key, hash_table):
+  hash_key = hash(key) % len(hash_table)
+  return hash_table[hash_key]    
+
+def insert3(hash_table, key, value):
+    key_exists = False
+    bucket = find_bucket(key, hash_table)
+    for i, key_value_pair in enumerate(bucket):
+        current_key, current_value = key_value_pair
+        if key == current_key:
+            key_exists = True 
+            break
+    if key_exists:
+        bucket[i] = ((key, value))
+    else:
+        bucket.append((key, value))
+
+insert3(hash_table3, 10, 'Ten')
+insert3(hash_table3, 20, 'Twenty')
+insert3(hash_table3, 25, 'Twenty-five')
+
+print(hash_table3)
+# -> [[(10, 'Ten'), (20, 'Twenty')], [], [], [], [], [(25, 'Twenty-five')], [], [], [], []]
+
+def search(hash_table, key):
+    bucket = find_bucket(key, hash_table)
+    for i, key_value_pair in enumerate(bucket):
+        current_key, current_value = key_value_pair
+        if key == current_key:
+            return current_value
+
+print(search(hash_table3, 10))
+# -> Ten
+print(search(hash_table3, 20))
+# -> Twenty
+
+# The above implementation is O(N), where N is number of keys.
