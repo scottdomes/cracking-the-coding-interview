b88ec82deb3b032a64130f166b5159e8275926a1
Scott <scottdomes@gmail.com>
2021-03-16 23:51:11 +0000

    Rust ownership troubles

--- chapter-1/questions/palindrome.rs
+++ chapter-1/questions/palindrome.rs
@@ -1,6 +1,35 @@
-fn main() {
-    // Statements here are executed when the compiled binary is called
+fn is_palindrome_permutation(string: &str) -> bool {
+  let lowercased = string.to_lowercase();
+  let chars = lowercased.chars();
+  chars.filter(|c| !c.is_whitespace());
+  // let filtered = cleaned_string.filter(|c| !c.is_whitespace());
+  let mut odd_numbered_letter = false;
+  for character in chars {
+    let occurences = string.matches(character).count();
+    let is_odd = occurences % 2 != 0;
+    // println!("{}", is_odd);
+    // println!("{}", character);
 
-    // Print text to the console
-    println!("Hello World!");
+    if is_odd {
+      if odd_numbered_letter {
+        return false;
+      } else {
+        odd_numbered_letter = true;
+      }
+    }
+  }
+
+  return true;
+}
+
+fn main() {
+  let string1 = "TtcctT"; // True
+  let string2 = "TtctT"; // True
+  let string3 = "Tact Coa"; // True
+  let string4 = "taco catr"; // False
+  println!("{}", is_palindrome_permutation(string1));
+  println!("{}", is_palindrome_permutation(string2));
+  println!("{}", is_palindrome_permutation(string3));
+  println!("{}", is_palindrome_permutation(string4));
+
 }
