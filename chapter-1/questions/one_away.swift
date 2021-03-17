29cd38a9c66541a720d2721adea8a89081d74095
Scott <scottdomes@gmail.com>
2021-03-17 21:49:45 +0000

    Done one away

--- chapter-1/questions/one_away.swift
+++ chapter-1/questions/one_away.swift
@@ -1,7 +1,34 @@
 // There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one or zero edits away.
 
+func is_one_insert_away(string1: String, string2: String) -> Bool {
+  return string2.count - 1 == string1.count
+}
+
+func is_one_delete_away(string1: String, string2: String) -> Bool {
+  return string2.count + 1 == string1.count
+}
+
+func count_different_characters(string1: String, string2: String) -> Int {
+  var different_characters = 0
+  for character in string1 {
+    if !string2.contains(character) {
+      different_characters += 1
+    }
+  }
+
+  return different_characters
+}
+
+func is_one_replace_away(string1: String, string2: String) -> Bool {
+  if string1.count != string2.count {
+    return false
+  }
+
+  return count_different_characters(string1: string1, string2: string2) == 1
+}
+
 func is_one_away(string1: String, string2: String) -> Bool {
-  return true
+  return string1 == string2 || is_one_insert_away(string1: string1, string2: string2) || is_one_delete_away(string1: string1, string2: string2) || is_one_replace_away(string1: string1, string2: string2)
 }
 
 let example_string = "pale"
